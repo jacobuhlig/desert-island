8a5e0fb23c4c19200cf2eebc1d4b36f852b7efb3
jacobuhlig@gmail.com <birth_chubby_GLEN_thrips>
2021-09-17 15:29:06 +0200

    Array created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        //Declarations
+        int[] choiceOfItemStatistic = {10};
+
+        String[] choiceOfItem = {
+                "Phone", "Family album", "A bottle of water", "A knife", "Sunblock",
+                "Twine", "A fishing net", "A box of matches", "A bottle of jack", "A gun"};
+
+        //Text to prompt user
+        System.out.println("\nIf you were to be stranded on a desert island," +
+                "\nwhat item would you bring out of the following ten?\n");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i + ":\t" + choiceOfItem[i-1]);
+        }
+
+        System.out.println("\nInput choice: ");
+
+        for (int i = 0; i < 11; i++) {
+
+        }
+        choiceOfItemStatistic[0] = scan.nextInt();
+
+
+
 
     }
 }
